b5935b964dd5fdbea26a7fa35ada907608aec85d
Fernando <fernandoaugustocostapereira@gmail.com>
2022-06-25 23:18:57 -0400

    Primeira pagina de registro adicionada

---
+++ src/pages/Registrar/CabecalhoRegistrar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Registrar.css";
+
+function CabecalhoRegistrar() {
+    return(
+        <div id="cabecalho">
+            <h1> Costa $ Santos</h1>
+        </div>
+
+    );
+}
+
+export default CabecalhoRegistrar;
--- src/pages/Registrar/Registrar.js
+++ src/pages/Registrar/Registrar.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "../Registrar/Registrar.css";
+import CabecalhoRegistrar from "./CabecalhoRegistrar";
 
 function Registrar() {
+    
+    const voltar = () => {
+        //voltar para pagina inicial
+        window.history.back();
+    }
 
     return (
         <>
-            <div id="corpo">
-                <div id="lado-esquedo-questionaio">
+          < CabecalhoRegistrar />
+            <div id="corpo">   
+                <div id="titulo">
+                    <h1>Registrar</h1>
+                </div>
+                <div id="questionario">
                     <input type="text" placeholder="Digite seu nome" />
                     <input type="password" placeholder="Digite seu E-mail" />
                     <input type="password" placeholder="Confirme seu E-mail" />
                     <input type="password" placeholder="Digite sua senha" />
                     <input type="password" placeholder="Confirme sua senha" />
                 </div>
-                <div id="lado-direito-questionaio">
-                    <h1 id="fase1"> 1 </h1>
-                    <h1 id="fase2"> 2 </h1>
-                    <h1 id="fase3"> 3 </h1>
+                <div className="navegação">
+                    <button className="botao-voltar" onClick={voltar}>Voltar</button>
+                    <button className="botao-prosseguir">Continuar</button>
                 </div>
             </div>
         </>
